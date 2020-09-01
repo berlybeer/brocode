6d120cb9d9262046331a569374bc8b57c7e927e7
berlybeer <b.pumacajia.cz@gmail.com>
2020-09-01 10:18:27 -0500

    Java expressions

--- codeBro/src/codeBro/Main.java
+++ codeBro/src/codeBro/Main.java
@@ -4,70 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//*****Variables****////
+		int x = 5;
+		int age = 75;
 		
-//		//Step 1 Declaration
-//		//Step 2 Assignment
-//		//Step 2+1 Initialization
-//		int x = 5;
-//		int age = 75;
-//		
-//		//Data type + name = value;
-//		byte exeample1 = 1;			//byte integer betwwen 127 to -128
-//		short example2 = 2;			//short integer between 32,767 to -32,768
-//		int example3 = 3; 			//int integer betwwen +- 2,147,483,647
-//		long example4 = 4; 			//long integer +- 9 quintillion
-//		
-//		float example5 = 1.0f; 		//float decimanl with 32-bits precision
-//		double example6 = 1.0;		//double decimal with 64-bits precision
-//		
-//		char example7 = 'A';		//char a single character
-//		String example8 = "Hello";   //String a series of characters
-//				
-//		boolean example9 = true;	//boolean a true or false value
-//		
-//		int year = 2020;
-//		String make = "Ford";
-//		String model = "Mustang";
-//		char status = 'N';
-//		double mpg = 32.5;
-//		boolean forSale = true;
-//		String name = "Berly";
-//		
-//				
-//		System.out.println("*****Car Dealership******");
-//		System.out.println();
-//		System.out.println("year: \t\t"+ year);
-//		System.out.println("make: \t\t"+ make);
-//		System.out.println("model: \t\t"+ model);
-//		System.out.println("status: \t" + status);
-//		System.out.println("mpg: \t\t" + mpg);
-//		System.out.println("best regards " + name);
-//		System.out.println();
-//		System.out.println("************");
-//		
-////		
-////		int x;
-////		x = 5;
-//		System.out.print(x + "\n");
-//		System.out.println("Your age is: " + age + " Ok, boomer");
+		System.out.print(x + "\n");
+		System.out.println("Your age is: " + age + " Ok, boomer");
+
 		
-		///*****Swap to variables*****//
-//		
-//		String x = "water";
-//		String y = "Kool-Aid";
-//		String temp;
-//		
-//		temp = x;
-//		x=y;
-//		y=temp;
-//		
-//	
-//		System.out.println("x: "+x);
-//		System.out.println("y: "+y);
+
 		
-		
-		///***
 		
 	}
 
--- codeBro/src/codeBro/variables.java
+++ codeBro/src/codeBro/variables.java
@@ -1,5 +1,54 @@
 package codeBro;
 
 public class variables {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//*****Variables****////
+		
+		//Step 1 Declaration
+		//Step 2 Assignment
+		//Step 2+1 Initialization
+	
+		
+		//Data type + name = value;
+		byte exeample1 = 1;			//byte integer betwwen 127 to -128
+		short example2 = 2;			//short integer between 32,767 to -32,768
+		int example3 = 3; 			//int integer betwwen +- 2,147,483,647
+		long example4 = 4; 			//long integer +- 9 quintillion
+		
+		float example5 = 1.0f; 		//float decimanl with 32-bits precision
+		double example6 = 1.0;		//double decimal with 64-bits precision
+		
+		char example7 = 'A';		//char a single character
+		String example8 = "Hello";   //String a series of characters
+		
+		boolean example9 = true;	//boolean a true or false value
+		
+		int year = 2020;
+		String make = "Ford";
+		String model = "Mustang";
+		char status = 'N';
+		double mpg = 32.5;
+		boolean forSale = true;
+		String name = "Berly";
+		
+		
+		System.out.println("*****Car Dealership******");
+		System.out.println();
+		System.out.println("year: \t\t"+ year);
+		System.out.println("make: \t\t"+ make);
+		System.out.println("model: \t\t"+ model);
+		System.out.println("status: \t" + status);
+		System.out.println("mpg: \t\t" + mpg);
+		System.out.println("best regards " + name);
+		System.out.println();
+		System.out.println("************");
+	}
+	
+	
+
 
 }
