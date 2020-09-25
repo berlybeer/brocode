04cbe4f9ff6404843203c443c3064ee95b2c7de3
berlybeer <b.pumacajia.cz@gmail.com>
2020-09-25 13:26:00 -0500

    Java object oriented programming OOP

---
+++ codeBro/src/OOP/Main.java
@@ -0,0 +1,30 @@
+package OOP;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//object = an instance of a class that may contain attributes
+		//example: (phone, celphone, desk, coffe, etc)
+		
+		
+		
+		Car myCar1 = new Car();
+	
+		
+		System.out.println(myCar1.model);
+		System.out.println(myCar1.make);
+		myCar1.drive();
+		
+		System.out.println();
+		Car myCar2 = new Car();
+		
+		System.out.println(myCar2.model);
+		System.out.println(myCar2.make);
+		
+		
+		
+	}
+
+}
