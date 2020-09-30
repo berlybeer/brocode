6b442a948b0d42fb51c54da6830aff153e6ce59f
berlybeer <b.pumacajia.cz@gmail.com>
2020-09-30 09:58:40 -0500

    Java static keyword

---
+++ codeBro/src/staticKeyword/Main.java
@@ -0,0 +1,26 @@
+package staticKeyword;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Friend friend1 = new Friend("Sponegbob");
+		Friend friend2 = new Friend("Patrick");
+		Friend friend3 = new Friend("Squidward");
+		Friend friend4 = new Friend("Berly");
+		
+		Friend friend5 = new Friend("Sandy");
+		
+		
+//		System.out.println(Friend.numberOfFriends);
+		
+		
+		Friend.displayFriends();
+		
+//		Math math = new Math();
+//		Math.round(a);
+	}
+
+}
