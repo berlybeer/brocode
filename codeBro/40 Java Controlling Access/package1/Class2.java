927e480b692c51d4e5f8a9b027520260bad1c62c
berlybeer <b.pumacajia.cz@gmail.com>
2020-12-15 16:22:15 -0500

    Java Controlling Access

---
+++ codeBro/40 Java Controlling Access/package1/Class2.java
@@ -0,0 +1,17 @@
+package package1;
+
+public class Class2 {
+	
+	private static char secretLetter = '?';
+
+	public static char getSecretLetter() {
+		return secretLetter;
+	}
+
+	public static void setSecretLetter(char secretLetter) {
+		Class2.secretLetter = secretLetter;
+	}
+	
+	
+
+}
---
+++ codeBro/40 Java Controlling Access/package2/Class3.java
@@ -0,0 +1,13 @@
+package package2;
+
+import package1.Class2;
+
+public class Class3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Class2.getSecretLetter());
+
+	}
+
+}
