c2451e10f4070bd65530ab57e6f8dee3f096c747
berlybeer <b.pumacajia.cz@gmail.com>
2020-12-17 16:02:51 -0500

    Java GUI

---
+++ codeBro/49 Java GUI/JavaGUI/Main.java
@@ -0,0 +1,31 @@
+package JavaGUI;
+
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// JFrame = a GUI window to add components to
+//		
+//		JFrame frame = new JFrame(); //creates a frame
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application
+//		frame.setResizable(false); //prevent frame from being resized
+//		frame.setSize(420, 420); //sets the x-dimension, and y-dimension of frame
+//		frame.setVisible(true); //make frame visible
+//		
+//		
+//		ImageIcon image = new ImageIcon("Logo.png"); //Create an ImageIcon
+//		frame.setIconImage(image.getImage()); //Change icon of frame
+//		frame.getContentPane().setBackground(new Color(123,50,250));
+		
+		MyFrame myFrame = new MyFrame();
+
+	}
+
+}
