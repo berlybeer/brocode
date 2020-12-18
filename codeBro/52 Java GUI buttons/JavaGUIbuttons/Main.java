8b51f8ec4f12b9d09bd68e40ee95b5adb6d2d068
berlybeer <b.pumacajia.cz@gmail.com>
2020-12-18 18:49:55 -0500

    Java GUI buttons

---
+++ codeBro/52 Java GUI buttons/JavaGUIbuttons/Main.java
@@ -0,0 +1,12 @@
+package JavaGUIbuttons;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		//JButton = a button that performs an action
+		
+		MyFrame frame = new MyFrame();
+	}
+
+}
